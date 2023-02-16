d403803885860b9a348aa79a4612bde32fc45976
brightateko <atekobright@yahoo.com>
2023-02-16 13:55:50 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*main-Prints "Programming is like building
+ *             a multilingual puzzle"
+ *Return: Always 0
+ */
+
+int man(void)
+{ 
+	puts ("\"Programming is like building a multilingual puzzle");
+
+	reutrn (0);
+}
+
