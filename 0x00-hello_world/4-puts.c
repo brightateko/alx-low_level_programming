89d6179b07ab914a2284b43a1b4e05bb3b784c83
brightateko <atekobright@yahoo.com>
2023-02-16 14:15:14 -0800

    stress

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-/*main-Prints "Programming is like building
- *             a multilingual puzzle"
- *Return: Always 0
+/**
+ * main - Prints "Programming is like building a multilingual 
+ *                puzzle, Followed by a new line.
+ *
+ * Return: Always 0.
  */
+
+
 
 int main(void)
 { 
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
